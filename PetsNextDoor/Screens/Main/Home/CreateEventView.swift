cc9329109f1ff536bb89da456756012d4e556676
youngchae <kevinkim2586@weversecompany.com>
2024-10-24 22:10:14 +0900

    implement expandableView

---
+++ PetsNextDoor/Screens/Main/Home/CreateEventView.swift
@@ -0,0 +1,8 @@
+//
+//  CreateEventView.swift
+//  PetsNextDoor
+//
+//  Created by kevinkim2586 on 10/24/24.
+//
+
+import Foundation
---
+++ PetsNextDoor/Screens/ReusableViews/SwiftUI_Views/ExpandableView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpandableView.swift
+//  PetsNextDoor
+//
+//  Created by kevinkim2586 on 10/24/24.
+//
+
+import SwiftUI
+
+struct ExpandableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpandableView()
+}
