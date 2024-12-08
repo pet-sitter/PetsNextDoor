325c714bd3dcd8d6ff8f4b386f637a84c28987a4
z3rosmith <52317025+z3rosmith@users.noreply.github.com>
2024-12-08 22:21:05 +0900

    remove MockChatLiveService not using anymore

--- PetsNextDoor/Screens/Main/Chat/LiveChatService.swift
+++ PetsNextDoor/Screens/Main/Chat/LiveChatService.swift
@@ -74,10 +74,6 @@ final class LiveChatService: ChatServiceProvidable, SocketServiceProvidable {
     configuration: Configuration
   ) {
     self.configuration = configuration
-//    request.setValue(
-//      "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjFkYmUwNmI1ZDdjMmE3YzA0NDU2MzA2MWZmMGZlYTM3NzQwYjg2YmMiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoia2V2aW5raW0yNTg2IiwicGljdHVyZSI6Imh0dHA6Ly9rLmtha2FvY2RuLm5ldC9kbi9TRlN1Vy9idHNJTTdQVGx3RS9wZUgzVXFZaEtiNzh5bGVpZnNmNGwxL2ltZ182NDB4NjQwLmpwZyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9wZXRzbmV4dGRvb3ItNzg4YTAiLCJhdWQiOiJwZXRzbmV4dGRvb3ItNzg4YTAiLCJhdXRoX3RpbWUiOjE3MjI3NDE2NjcsInVzZXJfaWQiOiIzMzk4MDQ4Mjk5Iiwic3ViIjoiMzM5ODA0ODI5OSIsImlhdCI6MTcyMjc0MTY2NywiZXhwIjoxNzIyNzQ1MjY3LCJlbWFpbCI6Imtldmlua2ltMjU4NkBrYWthby5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsia2V2aW5raW0yNTg2QGtha2FvLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6ImN1c3RvbSJ9fQ.tnJdu_7Js20QDDmbjtG87w10VFOPI3yjaQUAGgTVN8TD4XGhO2n7C5uFpzlC5vfcsSEm07uV8JAKptu0d5--G8HADJdFDkucy89mLLBFsw9J4_F0sbI-4YJKd5Y-E_lGf6SPK3AUnMRxMW9bu9nWFpnjHU7CqClgQ3uY0Qrqxnz7qJMM1KRebaim57VLnsP88sZa9ot5mSWUq24bl0TjPWOgprSHC1_4PhRiGaapXo0yd1a8XRb-NgU5lGdyTbIHoKtTLZ_qBjgdcet2GCPyxR7HGl_HG2_Cay1csKq9ywuthNYQKvC8e6Eib_29q4h1VuraerWg-gDqzmpVm4Q-3g",
-//      forHTTPHeaderField: "Authorization"
-//    )
     
     switch type {
     case .product(let url):
@@ -190,61 +186,6 @@ final class LiveChatService: ChatServiceProvidable, SocketServiceProvidable {
     }
     
     socket.write(string: chatJSON)
-  }
-}
-
-
-final class MockLiveChatService: ChatServiceProvidable {
-  
-  weak var delegate: (any ChatServiceDelegate)?
-  
-  private var timerSubscription: AnyCancellable?
-  
-  init() {
-  }
-  
-  func connect() {
-    beginGeneratingMockChatMessages()
-  }
-  
-  func disconnect() {
-    timerSubscription?.cancel()
-    timerSubscription = nil
-  }
-  
-  func sendMessage(_ message: String) {
-    
-  }
-  
-  func sendImages(mediaIds: [String]) {
-    
-    
-  }
-  
-  private func beginGeneratingMockChatMessages() {
-    
-    timerSubscription = Timer
-      .publish(every: 3.5, on: .main, in: .common)
-      .autoconnect()
-      .sink { [weak self] _ in
-        self?.delegate?.onReceiveNewText(PND.ChatModel(
-          room: .init(id: "1"),
-          messageType: [PND.MessageType.media, PND.MessageType.plain].randomElement()!.rawValue,
-          message: MockDataProvider.chatBubbleViewModels.map(\.body).randomElement()!,
-          messageId: UUID().uuidString,
-          medias: []
-        ))
-//        self?.delegate?.onReceiveNewText(
-//          MockDataProvider.chatBubbleViewModels.map(\.body).randomElement()!
-//        )
-      }
-    
-//    delegate?.onConnect()
-  }
-  
-  func stopGeneratingMockChatMessages() {
-    timerSubscription?.cancel()
-    timerSubscription = nil
   }
 }
 
