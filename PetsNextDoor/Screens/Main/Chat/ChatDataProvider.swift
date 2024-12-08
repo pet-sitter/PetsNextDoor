0fb3cd6818b594ff16d7aed927e5d502e2ff3b62
z3rosmith <52317025+z3rosmith@users.noreply.github.com>
2024-12-08 12:21:35 +0900

    fix indent

--- PetsNextDoor/Screens/Main/Chat/ChatDataProvider.swift
+++ PetsNextDoor/Screens/Main/Chat/ChatDataProvider.swift
@@ -21,14 +21,14 @@ final class ChatDataProvider {
   }
   
   private let configuration: Configuration
-
+  
   private var chatSocketService: any ChatServiceProvidable
   private var chatAPIService: any ChatAPIServiceProvidable
   private let mediaService: any MediaServiceProvidable
   
   private(set) var chatModels: [PND.ChatModel] = []
-	
-	private var continuation: AsyncStream<Action>.Continuation!
+  
+  private var continuation: AsyncStream<Action>.Continuation!
   
   init() {
     self.configuration = Configuration(roomId: "f5e17d20-5688-4924-b6c7-4509e80f04ad")
@@ -46,17 +46,17 @@ final class ChatDataProvider {
   func fetchRoomInfo() async throws -> PND.ChatRoomModel {
     return try await chatAPIService.getChatRoom(roomId: configuration.roomId)
   }
-	
-	func observeChatActionStream() -> AsyncStream<Action> {
-		return AsyncStream<Action> { continuation in
-			continuation.onTermination = { [weak self] _ in
+  
+  func observeChatActionStream() -> AsyncStream<Action> {
+    return AsyncStream<Action> { continuation in
+      continuation.onTermination = { [weak self] _ in
         self?.chatSocketService.disconnect()
-			}
-    
-			self.continuation = continuation
-			chatSocketService.connect()
-		}
-	}
+      }
+      
+      self.continuation = continuation
+      chatSocketService.connect()
+    }
+  }
   
   func sendChat(text: String) {
     chatSocketService.sendMessage(text)
@@ -82,17 +82,17 @@ final class ChatDataProvider {
 
 
 extension ChatDataProvider: ChatServiceDelegate {
-	
-	func onConnect() {
-		continuation.yield(.onConnect)
-	}
-	
-	func onDisconnect() {
-		continuation.yield(.onDisconnect)
-	}
+  
+  func onConnect() {
+    continuation.yield(.onConnect)
+  }
+  
+  func onDisconnect() {
+    continuation.yield(.onDisconnect)
+  }
   
   func onReceiveNewUser() {
-		
+    
   }
   
   func onReceiveNewText(_ chatModel: PND.ChatModel) {
@@ -112,7 +112,7 @@ extension ChatDataProvider: ChatServiceDelegate {
           } else { // 마지막 채팅이 상대방이 보낸 채팅이면
             chatViewTypes.append(ChatViewType.spacer(height: 20))
           }
-        
+          
         } else {  // 첫번째 말풍선인 경우
           chatViewTypes.append(ChatViewType.spacer(height: 4))
         }
@@ -143,9 +143,9 @@ extension ChatDataProvider: ChatServiceDelegate {
         default:
           break
         }
-				// Jin - ???: 아래 부분만 serial하게 일어나면 되지않을까?
+        // Jin - ???: 아래 부분만 serial하게 일어나면 되지않을까?
         chatModels.append(chatModel)
-				// Jin - 이 부분은 받는 쪽에서 await으로 어짜피 처리되니깐 serial안해도될거같고..
+        // Jin - 이 부분은 받는 쪽에서 await으로 어짜피 처리되니깐 serial안해도될거같고..
         continuation.yield(.onReceiveNewChatType(chatViewTypes))
       }
     }
