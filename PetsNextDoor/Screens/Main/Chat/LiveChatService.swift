f8ced407718c5fc408a39369f4414c4925687ad1
z3rosmith <52317025+z3rosmith@users.noreply.github.com>
2024-12-08 12:21:35 +0900

    fix indent

--- PetsNextDoor/Screens/Main/Chat/LiveChatService.swift
+++ PetsNextDoor/Screens/Main/Chat/LiveChatService.swift
@@ -15,28 +15,28 @@ import Starscream
 protocol ChatServiceProvidable {
   
   var delegate: (any ChatServiceDelegate)? { get set }
-	
-	func connect()
+  
+  func connect()
   func disconnect()
   func sendMessage(_ message: String)
   func sendImages(mediaIds: [String])
 }
 
 protocol ChatServiceDelegate: AnyObject {
-	
-	func onConnect()
-	func onDisconnect()
-	func onReceiveNewUser()
-	func onReceiveNewText(_ chatModel: PND.ChatModel)
+  
+  func onConnect()
+  func onDisconnect()
+  func onReceiveNewUser()
+  func onReceiveNewText(_ chatModel: PND.ChatModel)
 }
 
 // MARK: - SocketService
 
 protocol SocketServiceProvidable: WebSocketDelegate {
-	
+  
   associatedtype Configuration
   
-	var socket: any PNDWebSocketClient { get }
+  var socket: any PNDWebSocketClient { get }
   
   var configuration: Configuration { get }
 }
@@ -44,7 +44,7 @@ protocol SocketServiceProvidable: WebSocketDelegate {
 protocol PNDWebSocketClient: WebSocketClient & WebSocketDelegateProvidable {}
 
 protocol WebSocketDelegateProvidable {
-	var delegate: (any WebSocketDelegate)? { get set }
+  var delegate: (any WebSocketDelegate)? { get set }
 }
 
 extension WebSocket: PNDWebSocketClient {}
@@ -53,8 +53,8 @@ extension WebSocket: PNDWebSocketClient {}
 // MARK: -
 
 enum LiveChatServiceType {
-	case live(URL)
-	case mock
+  case live(URL)
+  case mock
 }
 
 final class LiveChatService: ChatServiceProvidable, SocketServiceProvidable {
@@ -63,7 +63,7 @@ final class LiveChatService: ChatServiceProvidable, SocketServiceProvidable {
     let roomId: String
   }
   
-	private(set) var socket: any PNDWebSocketClient
+  private(set) var socket: any PNDWebSocketClient
   
   private(set) var configuration: Configuration
   
@@ -72,7 +72,7 @@ final class LiveChatService: ChatServiceProvidable, SocketServiceProvidable {
   @Published private(set) var isConnected: Bool = false
   
   init(
-		type: LiveChatServiceType,
+    type: LiveChatServiceType,
     configuration: Configuration
   ) {
     self.configuration = configuration
@@ -80,16 +80,16 @@ final class LiveChatService: ChatServiceProvidable, SocketServiceProvidable {
 //      "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjFkYmUwNmI1ZDdjMmE3YzA0NDU2MzA2MWZmMGZlYTM3NzQwYjg2YmMiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoia2V2aW5raW0yNTg2IiwicGljdHVyZSI6Imh0dHA6Ly9rLmtha2FvY2RuLm5ldC9kbi9TRlN1Vy9idHNJTTdQVGx3RS9wZUgzVXFZaEtiNzh5bGVpZnNmNGwxL2ltZ182NDB4NjQwLmpwZyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9wZXRzbmV4dGRvb3ItNzg4YTAiLCJhdWQiOiJwZXRzbmV4dGRvb3ItNzg4YTAiLCJhdXRoX3RpbWUiOjE3MjI3NDE2NjcsInVzZXJfaWQiOiIzMzk4MDQ4Mjk5Iiwic3ViIjoiMzM5ODA0ODI5OSIsImlhdCI6MTcyMjc0MTY2NywiZXhwIjoxNzIyNzQ1MjY3LCJlbWFpbCI6Imtldmlua2ltMjU4NkBrYWthby5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsia2V2aW5raW0yNTg2QGtha2FvLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6ImN1c3RvbSJ9fQ.tnJdu_7Js20QDDmbjtG87w10VFOPI3yjaQUAGgTVN8TD4XGhO2n7C5uFpzlC5vfcsSEm07uV8JAKptu0d5--G8HADJdFDkucy89mLLBFsw9J4_F0sbI-4YJKd5Y-E_lGf6SPK3AUnMRxMW9bu9nWFpnjHU7CqClgQ3uY0Qrqxnz7qJMM1KRebaim57VLnsP88sZa9ot5mSWUq24bl0TjPWOgprSHC1_4PhRiGaapXo0yd1a8XRb-NgU5lGdyTbIHoKtTLZ_qBjgdcet2GCPyxR7HGl_HG2_Cay1csKq9ywuthNYQKvC8e6Eib_29q4h1VuraerWg-gDqzmpVm4Q-3g",
 //      forHTTPHeaderField: "Authorization"
 //    )
-		
-		switch type {
-		case .live(let url):
-			var request = URLRequest(url: url)
-			request.setValue(PNDTokenStore.shared.accessToken, forHTTPHeaderField: "Authorization")
-			socket = WebSocket(request: request)
-		case .mock:
-			socket = MockWebSocket()
-		}
-		socket.delegate = self
+    
+    switch type {
+    case .live(let url):
+      var request = URLRequest(url: url)
+      request.setValue(PNDTokenStore.shared.accessToken, forHTTPHeaderField: "Authorization")
+      socket = WebSocket(request: request)
+    case .mock:
+      socket = MockWebSocket()
+    }
+    socket.delegate = self
   }
   
   func didReceive(event: WebSocketEvent, client: any WebSocketClient) {
@@ -188,7 +188,7 @@ final class LiveChatService: ChatServiceProvidable, SocketServiceProvidable {
       PNDLogger.`default`.error("sendImages error encoding message")
       return
     }
-  
+    
     socket.write(string: chatJSON)
     
   }
@@ -196,7 +196,7 @@ final class LiveChatService: ChatServiceProvidable, SocketServiceProvidable {
 
 
 final class MockLiveChatService: ChatServiceProvidable {
-
+  
   weak var delegate: (any ChatServiceDelegate)?
   
   private var timerSubscription: AnyCancellable?
@@ -251,28 +251,28 @@ final class MockLiveChatService: ChatServiceProvidable {
 
 
 final class MockWebSocket: PNDWebSocketClient {
-	
-	var delegate: (any WebSocketDelegate)?
-	private var timerSubscription: AnyCancellable?
-	
-	func connect() {
-		timerSubscription = Timer
-			.publish(every: 3.5, on: .main, in: .common)
-			.autoconnect()
-			.sink { [weak self] _ in
-				guard let self else { return }
-				self.delegate?.didReceive(event: .text(MockDataProvider.textEvents.randomElement()!), client: self)
-			}
-	}
-	
-	func disconnect(closeCode: UInt16 = 0) {
-		timerSubscription?.cancel()
-		timerSubscription = nil
-	}
-	
-	func write(string: String, completion: (() -> ())?) {}
-	func write(stringData: Data, completion: (() -> ())?) {}
-	func write(data: Data, completion: (() -> ())?) {}
-	func write(ping: Data, completion: (() -> ())?) {}
-	func write(pong: Data, completion: (() -> ())?) {}
+  
+  var delegate: (any WebSocketDelegate)?
+  private var timerSubscription: AnyCancellable?
+  
+  func connect() {
+    timerSubscription = Timer
+      .publish(every: 3.5, on: .main, in: .common)
+      .autoconnect()
+      .sink { [weak self] _ in
+        guard let self else { return }
+        self.delegate?.didReceive(event: .text(MockDataProvider.textEvents.randomElement()!), client: self)
+      }
+  }
+  
+  func disconnect(closeCode: UInt16 = 0) {
+    timerSubscription?.cancel()
+    timerSubscription = nil
+  }
+  
+  func write(string: String, completion: (() -> ())?) {}
+  func write(stringData: Data, completion: (() -> ())?) {}
+  func write(data: Data, completion: (() -> ())?) {}
+  func write(ping: Data, completion: (() -> ())?) {}
+  func write(pong: Data, completion: (() -> ())?) {}
 }
